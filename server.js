51d4372f5abcf2b9c37a83e1e7dff8ae0e6b57a5
Christian Szandor Knapp <szandor72@gmail.com>
2016-08-15 11:59:24 +0200

    Added files

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var env = require('node-env-file');
+
+// Load environment variables for localhost
+try {
+	env(__dirname + '/.env');
+} catch (e) {}
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+var https_port = process.env.HTTPS_PORT || parseInt(port) + 1;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index', {appId: process.env.APPID, loApp: process.env.LOAPP});
+});
+
+app.get('/oauthcallback', function(req, res) {
+	res.render('oauthcallback', {});
+});
+
+// Create an HTTP service
+http.createServer(app).listen(port);
+console.log("Server listening for HTTP connections on port ", port);
+
+// Create an HTTPS service if the certs are present
+try {
+	var options = {
+	  key: fs.readFileSync('key.pem'),
+	  cert: fs.readFileSync('key-cert.pem')
+	};
+	https.createServer(options, app).listen(https_port);
+	console.log("Server listening for HTTPS connections on port ", https_port);
+} catch (e) {
+	console.error("Security certs not found, HTTPS not available");
+}
