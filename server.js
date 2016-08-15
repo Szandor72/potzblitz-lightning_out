1808fae2136bac625720ac7cc88cd73ccb092f54
Christian Szandor Knapp <szandor72@gmail.com>
2016-08-15 13:53:21 +0200

    oauth update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var env = require('node-env-file');
+var oauth2 = require('salesforce-oauth2');
 
 // Load environment variables for localhost
 try {
@@ -19,13 +20,59 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
+  var uri = oauth2.getAuthorizationUrl({
+      redirect_uri: process.env.CALLBACKURL,
+      client_id: process.env.APPID,
+      scope: 'full'
+  });
     res.render('index', {
         appId: process.env.APPID,
         loApp: process.env.LOAPP
     });
+  return response.redirect(uri);
 });
 
 app.get('/oauthcallback', function(req, res) {
+    var authorizationCode = request.param('code');
+
+    oauth2.authenticate({
+        redirect_uri: process.env.CALLBACKURL,
+        client_id: process.env.APPID,
+        client_secret: process.env.SECRET,
+        code: authorizationCode
+    }, function(error, payload) {
+        /*
+
+        The payload should contain the following fields:
+
+        id 				A URL, representing the authenticated user,
+                        which can be used to access the Identity Service.
+
+        issued_at		The time of token issue, represented as the
+                        number of seconds since the Unix epoch
+                        (00:00:00 UTC on 1 January 1970).
+
+        refresh_token	A long-lived token that may be used to obtain
+                        a fresh access token on expiry of the access
+                        token in this response.
+
+        instance_url	Identifies the Salesforce instance to which API
+                        calls should be sent.
+
+        access_token	The short-lived access token.
+
+
+        The signature field will be verified automatically and can be ignored.
+
+        At this point, the client application can use the access token to authorize requests
+        against the resource server (the Force.com instance specified by the instance URL)
+        via the REST APIs, providing the access token as an HTTP header in
+        each request:
+
+        Authorization: OAuth 00D50000000IZ3Z!AQ0AQDpEDKYsn7ioKug2aSmgCjgrPjG...
+        */
+    });
+    // have both authentications
     res.render('oauthcallback', {});
 });
 
